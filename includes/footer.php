9e76bc3b61005fe92968d626866b50fbf763c641
Christopher Buckley <chris@cbuckley.com>
2014-02-21 15:04:00 +0100

    Added footer to template management

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+  </body>
+</html>
